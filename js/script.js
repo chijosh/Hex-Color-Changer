5a071c4d45d7d78513bea19972384de5a9b19375
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-07-10 20:09:03 +0300

    modification update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,44 @@
-const colors = ["yellow", "green", "blue", "rgba(125,125,125,0.5)", "pink"];
+// const colors = ["yellow", "green", "blue", "rgba(125,125,125,0.5)", "pink"];
 
-const btn = document
-  .getElementById("btn")
-  .addEventListener("click", function() {
-    let random = Math.floor(Math.random() * colors.length);
-    const body = document.body;
-    body.style.backgroundColor = colors[random];
-    console.log(random);
+// const btn = document
+//   .getElementById("btn")
+//   .addEventListener("click", function() {
+//     let random = Math.floor(Math.random() * colors.length);
+//     const body = document.body;
+//     body.style.backgroundColor = colors[random];
+//     console.log(random);
+//   });
+
+(function() {
+  const hexValue = document.getElementById("hex-value");
+  const btn = document.getElementById("btn");
+
+  btn.addEventListener("click", function() {
+    const hexValues = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F"
+    ];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      let random = Math.floor(Math.random() * hexValues.length);
+      hexColor += hexValues[random];
+    }
+    document.body.style.backgroundColor = hexColor;
+    hexValue.textContent = hexColor;
   });
+})();
