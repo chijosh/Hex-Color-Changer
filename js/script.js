1466762b99a41825b807e94cdfda5e74525d8f84
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-07-10 18:07:51 +0300

    add event listeners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const colors = ["yellow", "green", "blue", "rgba(125,125,125,0.5)", "pink"];
+
+const btn = document
+  .getElementById("btn")
+  .addEventListener("click", function() {
+    let random = Math.floor(Math.random() * colors.length);
+    const body = document.body;
+    body.style.backgroundColor = colors[random];
+    console.log(random);
+  });
